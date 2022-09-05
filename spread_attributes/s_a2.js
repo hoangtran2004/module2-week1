97dee4339bbca3f0618d1a8a96e33483f247abc8
hoangtran2004 <tranhuyhoang58825@gmail.com>
2022-09-05 14:35:29 +0700

    s_a

---
+++ spread_attributes/s_a2.js
@@ -0,0 +1,43 @@
+//ex1
+// const car1 = ["AUDI", "BMW", "TATA", "MERCEDES"]
+// const car2 = [...car1];
+// const car3 = [...car1, "NISSAN", "TOYOTA"];
+// console.log(car1);
+// console.log(car2);
+// console.log(car3);
+
+//ex2
+// const Animal = ["DOG", "CAT"];
+// const Animal2 = ["MOUSE", "BIRD"];
+// const Animal3 = [...Animal, ...Animal2];
+// console.log(Animal3);
+
+//ex3
+// const Dog = {
+//     name: "cheems",
+//     weight: "30kg",
+//     color: "black",
+// }
+// const Dog2 = {...Dog};
+// console.log(Dog2);
+
+//ex4
+// const Dog1 = {
+//     name: "cheems",
+//     color: 'yellow'
+// }
+// const Dog2 = {
+//     name: 'doge',
+//     color: 'black',
+//     size: 'big'
+// }
+// const Dog3 = {
+//     ...Dog1, ...Dog2
+// }
+// console.log(Dog3)
+
+//ex5
+// const arr = [1, 2, 3, 4, 5];
+// const [a, b, c, ...number] = arr;
+// console.log(a, b, c);
+// console.log(number);

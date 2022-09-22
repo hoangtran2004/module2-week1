ee6853326658383271062f7dcfbe6c8eeeaf919c
hoangtran2004 <tranhuyhoang58825@gmail.com>
2022-09-22 22:59:34 +0700

    done

---
+++ destructruringAssigement/destructuring.js
@@ -0,0 +1,20 @@
+let profile = {
+
+    fname: 'Code',
+
+
+    lname: 'Gym',
+
+
+    bday: new Date('1979-01-02')
+
+
+}
+
+
+let fname = profile.fname;
+
+
+let lname = profile.lname;
+
+console.log([fname], [lname]);
